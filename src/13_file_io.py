62d9b5ac198744762baf35c7f6ab818b34432b9c
Chris King <cking5836@gmail.com>
2020-09-01 21:13:29 -0400

    All but cal.py done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,9 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+foo_file = open("foo.txt", "r")
+print(foo_file.read())
+foo_file.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,11 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+bar_file = open("bar.txt", "w")
+bar_file.write("This is nothing\n")
+bar_file.close()
+
+bar_file = open("bar.txt", "r")
+print(bar_file.read())
+bar_file.close()
+
